f41da8b9329f80dbfa5ef868d19f6449c34ee5c5
134n <goruji.ruji@gmail.com>
2024-05-19 22:45:07 +0900

    test.cs
    Delegate

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private delegate void CallBuck(string mes);
+
+    [SerializeField] private string moji;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+
+        CallBuck callBuck = CallBuckMes;
+
+        callBuck(moji);
+
+    }
+
+    void CallBuckMes(string mes)
+    {
+        Debug.Log(mes);
+    }
+}
